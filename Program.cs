fdb2e4f250d212e00d0c52bf51a2ed3d652897d5
dilber taştan <159853864+coeditaa@users.noreply.github.com>
2024-10-23 18:45:54 +0300

    path dev -c# DecisionConstructsIf-ElseIf-Ternary
    Developed with visual studio

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KararyapılarıIf_ElseIf_Ternary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time < 11)
+            {
+                Console.WriteLine("günaydın");
+            }
+           else if (time <= 18)
+            {
+                Console.WriteLine("iyi günler");
+
+            }
+            else
+            {
+                Console.WriteLine("iyi geceler");
+                string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+                Console.WriteLine(sonuc);
+            }
+        }
+    }
+}
